adcdc1a9de55520c5c9bfd6d786fd0a19b5bb6e5
ronald <ronald-farias@outlook.com>
2019-01-31 02:08:08 -0300

    add OperacaoFinanceiraError exception

--- exceptions/classes.py
+++ exceptions/classes.py
@@ -2,6 +2,11 @@ class SemSaudoError(Exception):
     def __str__(self):
         return 'Saudo insuficiente para efetuar a transação'
 
+
 class SemPermissaoError(Exception):
     def __str__(self):
         return 'O cliente não possui permissão para esta ação'
+
+
+class OperacaoFinanceiraError(Exception):
+    pass
