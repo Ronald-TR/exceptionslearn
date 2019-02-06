c8c97da9506b3cf49d41ba32210a7257512e4260
ronald <ronald-farias@outlook.com>
2019-02-06 10:33:23 -0300

    Alterado except para TypeError e add um num valido no argumento agencia

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from exceptions import SemSaudoError
 
 try:
     ContaCorrente(None, 0, 0)
-except AssertionError as e:
+except TypeError as e:
     print(e)
 
 cliente_1 = Cliente('Cliente 1', 123456789, 'Carpinteiro')
 cliente_2 = Cliente('Cliente 2', 123, 'Designer')
 
-cc_cliente_1 = ContaCorrente(cliente_1, '00', 9909)
-cc_cliente_2 = ContaCorrente(cliente_2,'01', 9909)
+cc_cliente_1 = ContaCorrente(cliente_1, 100, 9909)
+cc_cliente_2 = ContaCorrente(cliente_2, 101, 9909)
 
 try:
     cc_cliente_1.transferir(100, cc_cliente_2)
